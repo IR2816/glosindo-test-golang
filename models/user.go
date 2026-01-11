b84a780760a51a90161fbd826892d6936dfee33c
IR2816 <irfanthebb816@gmail.com>
2026-01-11 20:09:01 +0700

    fix 1

--- models/user.go
+++ models/user.go
@@ -10,7 +10,7 @@ import (
 type User struct {
 	ID         string     `gorm:"type:varchar(50);primaryKey" json:"id"`
 	Name       string     `gorm:"type:varchar(100);not null" json:"name"`
-	Email      string     `gorm:"type:varchar(100);uniqueIndex;not null" json:"email"`
+	Email      string     `gorm:"type:varchar(100);unique;not null" json:"email"`
 	Password   string     `gorm:"type:varchar(255);not null" json:"-"`
 	Phone      string     `gorm:"type:varchar(20)" json:"phone"`
 	Address    string     `gorm:"type:text" json:"address"`
